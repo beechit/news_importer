2f4bdb7396c0091b89f55b12dfbd57dfc87f04aa
Ruud Silvrants <ruud@beech.it>
2015-09-10 12:01:33 +0200

    [TASK] Load queryPath if not autoloaded with composer
    Require autoload files of queryPath if not loaded with composer

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,10 +2,13 @@
 if (!defined('TYPO3_MODE')) die ('Access denied.');
 
 
+$pathQueryLib = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) .
+	'Resources/Private/PHP/querypath/src/qp.php';
 // Add custom autoloader for QueryPath
-require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) .
-	'Resources/Private/PHP/querypath/src/qp.php');
 
+if(file_exists($pathQueryLib)) {
+	require_once($pathQueryLib);
+}
 
 if (TYPO3_MODE === 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][]
